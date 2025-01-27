1abf0bc4a5db16a2e267fb59541be159787f747f
PlumPapi <jaycob.branco@outlook.com>
2025-01-26 23:37:12 -0500

    week3 Lab
    lab 3

--- W25-Week1-Lab1/Assets/_Project/Scripts/InputReader.cs
+++ W25-Week1-Lab1/Assets/_Project/Scripts/InputReader.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using static InputSystem_Actions;
+
 namespace Platformer397
 {
     [CreateAssetMenu(fileName = "InputReader", menuName = "Scriptable Objects/InputReader")]
     public class InputReader : ScriptableObject, IPlayerActions
     {
         public event UnityAction<Vector2> Move = delegate {};
+
         InputSystem_Actions input;
         private void OnEnable()
         {
@@ -17,10 +19,12 @@ namespace Platformer397
                 input.Player.SetCallbacks(this);
             }
         }
+
         public void EnablePlayerActions()
         {
             input.Enable();
         }
+
         public void OnMove(InputAction.CallbackContext context) 
         {
             switch(context.phase)
@@ -30,10 +34,9 @@ namespace Platformer397
                     Move?.Invoke(context.ReadValue<Vector2>());
                     break;
                 default:
-                    Debug.Log("not input handled");
                     break;
             }
-
+            
         }
         public void OnLook(InputAction.CallbackContext context) {}
         public void OnAttack(InputAction.CallbackContext context) {}
